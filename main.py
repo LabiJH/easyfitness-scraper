a5c31d18feaac530b0af118aff9d7f78291abf61
Labinot <labinot@jaha.dev>
2025-07-07 20:37:11 +0200

    bug fixes

--- main.py
+++ main.py
@@ -14,14 +14,13 @@ client = discord.Client(intents=intents)
 URL = "https://easyfitness.club/studio/easyfitness-bornheim/"
 
 async def send_updates():
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, "html.parser")
-    meterbubble = soup.find_all("span", class_="meterbubble")
-    firstBubble = meterbubble[0] # for some reason there are TWO bubble Objects?
-    print(f'Logged in as {client.user}')
-    channel = client.get_channel(CHANNEL_ID)
-    
     while True:
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+        meterbubble = soup.find_all("span", class_="meterbubble")
+        firstBubble = meterbubble[0] # for some reason there are TWO bubble Objects?
+        print(f'Logged in as {client.user}')
+        channel = client.get_channel(CHANNEL_ID)
         await channel.send("Das Studio hat " + firstBubble.text + " Auslastung momentan")
         await asyncio.sleep(1800)  # Sleep for 30 minutes (1800 seconds)
 
